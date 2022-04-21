4574e9051e8d2bf3aa210767003a37a30184da59
Sumit Yadav <mackbook@Sumits-MacBook-Pro.local>
2022-04-21 17:54:27 +0530

    Get all books, and book by id and remove book by id.

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { BOOKS } from 'src/mocks/books.mock';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  books = BOOKS
+
+  // GET all books
+  getBooks(): Promise<any> {
+    return new Promise(resolve => {
+      resolve(this.books)
+    })
+  }
+  // GET book by id
+  getBook(bookId: number): Promise<any> {
+    return new Promise(resolve => {
+      const book = this.books.find(book => book.id === bookId)
+      if (!book) {
+        throw new HttpException("Book does not exists!", 400)
+      }
+      resolve(book)
+    })
+  }
+  // Delete the book
+  deleteBook(bookId: number): Promise<any> {
+    return new Promise(resolve => {
+      const index = this.books.findIndex(book => book.id === bookId)
+      if (index === -1) {
+        throw new HttpException("Book not found", 400)
+      }
+      this.books.splice(1, index)
+      resolve(this.books)
+    })
+  }
+}
